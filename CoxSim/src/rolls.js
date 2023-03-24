fec4a46bcfae19976134b5fee9d9bbcbc228a872
t_ <therm.cn@gmail.com>
2023-03-24 17:54:43 -0400

    Revert "initila push"
    This reverts commit b37d13375c418b1b910838c634a61337572e6159.

--- CoxSim/src/rolls.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.rollForDamage = void 0;
-function rollForDamage(maxHit) {
-    return Math.floor(Math.random() * (maxHit + 1));
-}
-exports.rollForDamage = rollForDamage;
-//# sourceMappingURL=rolls.js.map
--- CoxSim/src/rolls.ts
+++
@@ -1,3 +0,0 @@
-export function rollForDamage(maxHit: number): number {
-    return Math.floor(Math.random() * (maxHit + 1));
-}
