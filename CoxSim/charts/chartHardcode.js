9942a3f3033c451826bb01e83858d2b2d39a3ad5
t_ <therm.cn@gmail.com>
2023-03-24 22:53:14 -0400

    update chart.js version

--- CoxSim/charts/chartHardcode.js
+++ CoxSim/charts/chartHardcode.js
@@ -1,15 +1,28 @@
 var xValues = ["Set1", "Set2"];
-var yValues = [55, 49, ];
+var yValues = [55, 49 ];
 var barColors = ["red", "green"];
 
-new Chart("myChart", {
-  type: "bar",
-  data: {
-    labels: xValues,
-    datasets: [{
-      backgroundColor: barColors,
-      data: yValues
-    }]
+var chartData = {
+  labels: xValues,
+  datasets: [{
+      label: 'Count',
+      backgroundColor: 'rgba(255, 255, 0, 0.75)',
+      borderColor: 'rgba(200, 200, 200, 0.75)',
+      hoverBackgroundColor: 'rgba(255, 255, 0, 1)',
+      hoverBorderColor: 'rgba(200, 200, 200, 1)',
+      data: yValues,
+    }
+  ]
+};
+
+var barGraph = new Chart("myChart", {
+  type: 'bar',
+  data: chartData,
+  options: {
+    scales: {
+      y: {
+          beginAtZero: true
+      }
+    }
   },
-  options: {} 
 });
